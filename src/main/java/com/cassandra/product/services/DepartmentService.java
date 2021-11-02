e5b930ba2c59e9237ff218691f6e4a2750ab18fb
Glauco Todesco <glauco.todesco@gmail.com>
2021-11-02 14:47:03 -0300

    Add Put Department

--- src/main/java/com/cassandra/product/services/DepartmentService.java
+++ src/main/java/com/cassandra/product/services/DepartmentService.java
@@ -42,7 +42,13 @@ public class DepartmentService {
 		entity = repository.save(entity);
 		return new DepartmentDTO(entity);		
 	}
-
+	
+	public DepartmentDTO update(UUID id, DepartmentDTO dto) {
+		Department entity = getById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new DepartmentDTO(entity);		
+	}
 			
 	private void copyDtoToEntity(DepartmentDTO dto, Department entity) {
 		entity.setName(dto.getName());
